8120568676a361042c41dd4f1101b49b0e7cd2dd
EsraaFarhat <esraafarhat97@gmail.com>
2021-06-04 00:07:12 +0200

    Finish the Error page

--- src/components/ErrorComponent.vue
+++ src/components/ErrorComponent.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Error page</h1>
+    <div class="error-container">
+        <img src="../assets/img/error.png" alt="Error" class="image">
+        <h2 class="not-found">404 - Page Not Found!</h2>
+        <small class="sorry">Sorry, that page doesn't exist. What would you like to do?</small>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.error-container{
+    text-align: center;
+}
+
+.image{
+    margin-top: 6%;
+    margin-bottom: 2%;
+    width: 30%;
+    height: 250px;
+}
+.not-found{
+    color: #052A82;
+    font-weight: bold;
+}
+.sorry{
+    color: #899FC7;
+}
+
+</style>
