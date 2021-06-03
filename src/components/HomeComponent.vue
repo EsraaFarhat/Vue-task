1791bd1693317261a76d7de9e4c0ad36a1d2dd17
EsraaFarhat <esraafarhat97@gmail.com>
2021-06-03 23:12:36 +0200

    Finish the home page logic

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Home page</h1>
+    <h1>Welcome to {{ email }}</h1>
+    <button type="submit" @click="logout">logout</button>
 </template>
+<script>
+export default {
+    data:()=>({
+        email: ""
+    }),
+    methods:{
+        logout(){
+            localStorage.removeItem('logged_in_email')
+            this.$router.push('/login')
+        }
+    },
+    created(){
+        this.email = localStorage.getItem('logged_in_email')
+        if(this.email == null){
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,3 +1,10 @@
 <template>
     <h1>Login page</h1>
 </template>
+<script>
+export default {
+    created(){
+        // localStorage.setItem('logged_in_email', "mohamed@instabug.com")
+    }
+}
+</script>
