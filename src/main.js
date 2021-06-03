21e537837532063f065704e01d6d4fca71457b97
EsraaFarhat <esraafarhat97@gmail.com>
2021-06-03 23:17:00 +0200

    Modify the 404 route

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ import ErrorComponent from './components/ErrorComponent.vue'
 const routes = [
     { path: '/login', component: LoginComponent },
     { path: '/welcome', component: HomeComponent },
-    { path: '/:catchAll(.*)', component: ErrorComponent },
+    { path: '/404', component: ErrorComponent },
+    { path: '/:catchAll(.*)', redirect: '/404' },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
