1784bb826d04d75773453c514cc3d69eb4bf7813
EsraaFarhat <esraafarhat97@gmail.com>
2021-06-03 21:12:50 +0200

    Initialize the routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+import HomeComponent from './components/HomeComponent.vue'
+import LoginComponent from './components/LoginComponent.vue'
+import ErrorComponent from './components/ErrorComponent.vue'
+
+const routes = [
+    { path: '/login', component: LoginComponent },
+    { path: '/welcome', component: HomeComponent },
+    { path: '/:catchAll(.*)', component: ErrorComponent },
+];
+
+const router = createRouter({ history: createWebHistory(), routes });
+
+createApp(App).use(router).mount('#app')
